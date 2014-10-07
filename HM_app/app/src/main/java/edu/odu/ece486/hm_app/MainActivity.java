ce6498a187243fd53167137ab61499f47d70ed3f
astam006 <astam006@odu.edu>
2014-10-07 15:16:47 -0400

    Splash page and main menu activities
    A lot of files. Some of them are image files used for the main menu. The
    splash page is ugly and will of course be changed, however, it does
    compile and run.

--- HM_app/app/src/main/java/edu/odu/ece486/hm_app/MainActivity.java
+++ HM_app/app/src/main/java/edu/odu/ece486/hm_app/MainActivity.java
@@ -1,19 +1,44 @@
 package edu.odu.ece486.hm_app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);     // Default view to the splash page.
+
+        // Create a thread that sleeps for 5 seconds before transitioning to the
+        // main menu activity. An intent is basically a request for the app to
+        // change activities.
+        Thread splashTimer = new Thread() {
+          public void run(){
+              try{
+                  sleep(5000);
+                  Intent menuIntent = new Intent("edu.odu.ece486.hm_app.MENU");
+                  startActivity(menuIntent);
+              } catch (InterruptedException e) {
+                  e.printStackTrace();
+              } finally {
+                  finish();
+              }
+          }
+        };
+        splashTimer.start();        // Starting the thread.
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
