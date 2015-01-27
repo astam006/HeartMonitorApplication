b7c305f0488d5535987a493350296e582bf4d7db
astam006 <astam006@odu.edu>
2015-01-26 21:28:18 -0500

    Began creation of Results Activity

--- HM_app/app/src/main/java/edu/odu/ece486/hm_app/PressureSensor.java
+++ HM_app/app/src/main/java/edu/odu/ece486/hm_app/PressureSensor.java
@@ -6,17 +6,29 @@ package edu.odu.ece486.hm_app;
 public class PressureSensor {
 
     private int mmHg;
+    private int maxPressure;
 
     public PressureSensor() {
         mmHg = 0;
     }
 
     public void update(int newPressure) {
+        adjustMax(newPressure);
         mmHg = newPressure;
     }
 
     public int getPressure() {
         return mmHg;
+    }
+
+    /**
+     * Check the highest received input and store it in maxPressure.
+     * The highest received input will be used for the Test Results.
+     * @param newPressure
+     */
+    public void adjustMax(int newPressure) {
+        if(newPressure > mmHg)
+            maxPressure = newPressure;
     }
 
 }
