76dab4882aea7a876678b1ee51ca146751605ffd
astam006 <astam006@odu.edu>
2015-02-05 22:17:55 -0500

    Implemented Alert Dialog
    Successful implementation of Low Pressure Alert dialog on the new
    testing screen. Also, there is a transition improvement (extended pause)
    at the end of the test before displaying results.

--- HM_app/app/src/main/java/edu/odu/ece486/hm_app/TestActivityV2.java
+++ HM_app/app/src/main/java/edu/odu/ece486/hm_app/TestActivityV2.java
@@ -1,6 +1,7 @@
 package edu.odu.ece486.hm_app;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.AsyncTask;
@@ -9,6 +10,7 @@ import android.os.CountDownTimer;
 import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -25,7 +27,7 @@ public class TestActivityV2 extends Activity {
     private PressureProgressBar pressureProgressBar;
     private TextView pressureValueText;
     private TextView timerView;
-    private Handler progressBarHandler = new Handler();
+    private AlertDialog lowPressureAlert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +37,7 @@ public class TestActivityV2 extends Activity {
         app = (cBaseApplication)getApplication();
         app.sendBeginDataTransferCommand();
 
+        lowPressureAlert = createAlertDialog();
         timerView = (TextView) findViewById(R.id.timerTextView);
         pressureValueText = (TextView) findViewById(R.id.pressureValueTxt);
         pressureProgressBar = (PressureProgressBar) findViewById(R.id.pressureProgressBar);
@@ -55,8 +58,6 @@ public class TestActivityV2 extends Activity {
                 testOver = true;
                 timerView.setTextColor(Color.GREEN);
                 timerView.setText("Done!");
-                startActivity(new Intent(TestActivityV2.this, ResultsActivity.class));
-                TestActivityV2.this.finish();
             }
         }.start();
     }
@@ -70,8 +71,36 @@ public class TestActivityV2 extends Activity {
         pressureProgressBar = pBar;
         pressureProgressBar.setProgress(newPressure);
         pressureValueText.setText(Long.toString(newPressure) + " mmHg");
+        if(newPressure < 20)
+            lowPressureAlert.show();
+        else
+            lowPressureAlert.dismiss();
     }
 
+    private AlertDialog createAlertDialog() {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+        alertBuilder.setMessage("Keep pressure above 20mmHg!")
+                .setTitle("Low Pressure Warning!")
+                .setIcon(R.drawable.ic_warning)
+                .setCancelable(false);
+        AlertDialog alertDialog = alertBuilder.create();
+        return  alertDialog;
+    }
+
+    /**
+     * Method called at the end of the countdown to allow the user to
+     * see the 'Done!' message and prepare to look at results.
+     */
+    private void gracefulResultsTransition() {
+        try {
+            Thread.sleep(1500);
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            startActivity(new Intent(TestActivityV2.this, ResultsActivity.class));
+            TestActivityV2.this.finish();
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -132,6 +161,11 @@ public class TestActivityV2 extends Activity {
                 }
 
             }
+
+            // Before returning, dismiss any dialogs and initiate transition to
+            // the Results Activity.
+            lowPressureAlert.dismiss();
+            gracefulResultsTransition();
             return null;
         }
 
